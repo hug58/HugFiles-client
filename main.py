dace6672fab4bd763846efb4d68318d60d8444c3
Hugo <hugomontaez@email.com>
2023-08-26 14:42:42 -0400

    fix: clear and fix someone errors and tabs

--- main.py
+++ main.py
@@ -17,116 +17,88 @@ from utils.download import done,deleted,modified,created
 sio = socketio.AsyncClient()
 
 
-#######################URL del server#######################
-URL = 'http://localhost:5000'
-#URL = 'https://hug-files.herokuapp.com/'
-
-############################################################
-
+URL = os.getenv("URL")
 global messages_files
-messages_files = [] #guardar los mensajes del server y luego procesarlos, de esta forma evito re-enviar un archivo
+messages_files = [] 
+
 
 @sio.on('connect')
 async def connect():
-	global path_user
-	print('Connection established')
+    global path_user
+    print('Connection established')
 
 
 @sio.on('notify')
 async def on_notify(metadata):
-	global path_user 
-	data = json.loads(metadata)
-	path_user = data["path"]
-	print(data, type(data))
+    global path_user 
+    data = json.loads(metadata)
+    path_user = data["path"]
+    print(data, type(data))
 
 
 @sio.on('files')
 async def on_files(data):
-	global result
-	result = ''
-	try:
-		data = json.loads(data)
-		messages_files.append(data)
-
-		if data['status'] == 'created' or data['status'] == 'done':
-			result = done(URL,data)
-		elif data['status'] == 'modified':
-			if modified(data): 
-				result = created(URL,data)
-		elif data['status'] == 'delete':
-			result = deleted(data)
-
-	except TypeError as e:
-		print(e)
-
+    """ get all files from server """
+    global result
+    result = ''
+    try:
+        data = json.loads(data)
+        messages_files.append(data)
+        if data['status'] == 'created' or data['status'] == 'done':
+            result = done(URL,data)
+        elif data['status'] == 'modified':
+            if modified(data): 
+                result = created(URL,data)
+        elif data['status'] == 'delete':
+            result = deleted(data)
+    except TypeError as err:
+        #TODO: handle error
+        print(data, err)
 
 async def producer_file(message):
-
-	if message['status'] == 'created':
-		requests.post(message['url'],files = {'upload_file': open(message['path'],'rb')})	
-
-	elif message['status'] == 'modified':
-		pass
-
-	elif message['status'] == 'deleted':
-		pass
+    """ only upload files, TODO: modified and deleted files"""
+    if message['status'] == 'created':
+        requests.post(message['url'],files = {'upload_file': open(message['path'],'rb')})
+    elif message['status'] == 'modified':
+        pass
+    elif message['status'] == 'deleted':
+        pass 
 
 
 async def producer_handler(_path='data/files/', path_user=None):
-	print('Producing %s' % path_user)
-	monitorsystem = event_handler.EventHandler(URL, path_user)
-	observer = observers.Observer()
-
-	observer.schedule(monitorsystem, path=_path, recursive=True)
-	observer.start()
-
-	print('Iniciando Monitor')
-
-	while True:
-
-		if monitorsystem.message != {}:
-
-			# enviando msg al server
-			_message = monitorsystem.message
-			monitorsystem.message = {}
-
-			print(_message)
-
-
-			try:
-				messages_files.remove(_message)
-				#await sio.emit('notify',_message)
-				print('Ya notificado')
-
-			except ValueError:
-				#debo confimar que no se esta reenviando un archivo
-				monitorsystem.message = {}
-				await producer_file(_message)
-
-
-		else:
-			# Sino esperar 1 seg
-			await asyncio.sleep(1)
-
+    """Load monitor files and send notifications"""
+    monitorsystem = event_handler.EventHandler(URL, path_user)
+    observer = observers.Observer()
+    observer.schedule(monitorsystem, path=_path, recursive=True)
+    observer.start()
+    print('Loading Monitorsystem')
+    while True:
+        if monitorsystem.message != {}:
+            _message = monitorsystem.message
+            monitorsystem.message = {}
+            print(_message)
+            try:
+                messages_files.remove(_message)
+            except ValueError:
+                monitorsystem.message = {}
+                await producer_file(_message)
+        else:
+            await asyncio.sleep(1)
 
 async def main():
-	# Carpeta predeterminada (por el momento), la idea a futuro es que el usuario pueda configurar esto a su gusto
-	DEFAULT_FOLDER = 'data/files/'
-	# Generando la carpeta predeterminada sino existe
-	if not os.path.isdir(DEFAULT_FOLDER):
-		os.makedirs(DEFAULT_FOLDER, exist_ok=True)
-
-	await sio.connect(URL)
-	data = {"email":"prueba@mail.com"}
-	respond = requests.post(URL + "/token", json=data, headers={'Content-Type': 'application/json'})
-	if respond.status_code == 200:
-		path_user = respond.json()["path"]
-		print(path_user)
-	else:
-		return 
-
-	await producer_handler(_path=DEFAULT_FOLDER, path_user=path_user)
-	await sio.wait()
+    DEFAULT_FOLDER = os.getenv("DEFAULT_FOLDER")
+    if not os.path.isdir(DEFAULT_FOLDER):
+        os.makedirs(DEFAULT_FOLDER, exist_ok=True)
+    await sio.connect(URL)
+    data = {"email":os.getenv("EMAIL")}
+    respond = requests.post(URL + "/token", json=data, headers={'Content-Type': 'application/json'})
+    if respond.status_code == 200:
+        path_user = respond.json()["path"]
+        print(path_user)
+    else:
+        return 
+    await producer_handler(_path=DEFAULT_FOLDER, path_user=path_user)
+    await sio.wait()
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
